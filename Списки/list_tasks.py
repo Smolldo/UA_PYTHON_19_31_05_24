4588f63c6130dad5ab5d8dd78e5ad544609071b3
Mykhajlo Honyk <maximqwerbich@gmail.com>
2024-07-05 21:49:22 +0300

    Update list_tasks.py

--- Списки/list_tasks.py
+++ Списки/list_tasks.py
@@ -1,85 +1,144 @@
-a = ['Misha', 'Kolya', 'Petya']
-
-if 'Misha' in a:
-    print('Yes')
-
-if "John" not in a:
-    print('John is not on the list')
-
-
-for item in a:
-    print(f'Hello {item}, u just won 300$')
-
-
 # Написати програму, що рахує суму всіх елеметів у списку.
 a = [1,2,3,4,5]
 suma = 0
 for i in a:
     suma += i
 print(suma)
-print(sum(a))
 
 # Написати програму, що рахує добуток всіх елементів у списку.
-a = [1,2,3,4,5]
 dob = 1
-
 for i in a:
     dob *= i
 print(dob)
 
+# Написати програму, що перевірить порожній список чи ні.
+b = []
+
+if not b:
+    print('spisok porozhniy')
+else:
+    print('spisok ne porozhniy')
 
 # Написати програму, яка видалить зі списку 0-й, 5-й і 7-й елементи зі списку
-c = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'] #a,f,h
-
-c.pop(7) #del c[7]
-c.pop(5) #del c[5]
-c.pop(0) #del c[0]
+c = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+del c[7]
+del c[5]
+del c[0]
 print(c)
 
 # Написати програму, що дістає максимальне число зі списку.
-a = [1,2,3,4,5]
-max_num = a[0]
-
-for i in a:
-    if i > max_num:
-        max_num = i
-print(max_num)
-print(max(a))
+print(f'max num from {a}\n is {max(a)}')
 
 # Піднести кожен із елементів списку до кубу
-a = [1,2,3,4,5]
-# a1 = []
-# for i in a:
-#     a1.append(i**3)
-# print(a1)
-a1 = [i ** 3 for i in a]
-print(a1)
+
+cubed = [i**3 for i in a]
+print(cubed)
 
 # Видалити пусті строки зі списку.
-f = ['a', '', 'sfsdfs', " "]
-f1 = []
-
-for i in f:
-    item = i.strip() # прибирає пробіли
+d = ['a', ' ', 'sd', '']
+new_d = []
+for i in d:
+    item = i.strip()
     if item:
-        f1.append(item)
-print(f1)
+        new_d.append(item)
+print(new_d)
+
+# Написати програму, що дістає мінімальне число зі списку.
+print(f'min num from {a}\n is {min(a)}')
 
 # Написати програму, що прибере дублікати зі списку.
-b = [1,2,33,3,3,3,3,3,1,1,1,4,4,5,6,7,10,4,4,4]
-b1 = list(set(b)) # .set() - прибирає дублікати
-print(b1)
 
+f = [1,2,3,1,1,2,3,6,8,7]
+# f_n = []
+# for i in f:
+#     if i not in f_n:
+#         f_n.append(i)
+# print(f_n)
+f_n = list(set(f))
+print(f_n)
 
 # Написати програму, що порахує кількість додатніх елементів і списку та кількість від’ємних
-lst = [1,2,3,-4,-5,-6,7,8,-9]
+numbers = [3, -1, 7, -4, 2, -8, 5, -6, 9, -3]
+pos = 0
+neg = 0
 
-pos_n = 0
-neg_n = 0
-
-for i in lst:
+for i in numbers:
     if i > 0:
-        pos_n +=1
+        pos+=1
     else:
-        neg_n += 1
-print(f'pos nums = {pos_n}. neg nums = {neg_n}')
+        neg +=1
+print(f' pos = {pos}. neg = {neg}')
+
+
+# Якщо число в списку парне — додайте до нього 1. Якщо непарне — відніміть 2.
+new_list = []
+# [3, -1, 7, -4, 2, -8, 5, -6, 9, -3]
+for i in numbers:
+    if i % 2 == 0:
+        i = i +1
+    else:
+        i = i - 2
+    new_list.append(i)
+print(new_list)
+
+# Написати програму, що порахує кількість унікальних елементів у списку.
+f = [1,2,3,1,1,2,3,6,8,7]
+
+uniq = set(f)
+print(f'uniq elems in f = {len(uniq)}')
+
+# Написати програму, що виведе список, після видалення із нього непарних елементів
+f = [1,2,3,1,1,2,3,6,8,7]
+
+n = [i for i in f if i % 2 == 0]
+print(n)
+
+# Створіть програму, яка приймає список чисел та виводить на екран кожне друге число у списку.
+for i in range(1, len(numbers), 2):
+    print(numbers[i])
+
+
+# Написати програму, що виведе список, після видалення із нього парних елементів
+f = [1,2,3,1,1,2,3,6,8,7]
+
+n = [i for i in f if i % 2 != 0]
+print(n)
+
+# Створити програму, яка приймає список слів та повертає список слів у зворотньому порядку.
+c = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+print(c)
+c.reverse()
+print(c)
+
+# Створити програму, яка приймає список чисел та повертає список елементів, які є менші за середнє значення списку.
+f = [1,2,3,1,1,2,3,6,8,7]
+
+ser = sum(f) / len(f)
+nl = []
+for i in f:
+    if i < ser:
+        nl.append(i)
+print(nl)
+
+# Написати програму, яка знайде список слів, які довші за N(довільне) у заданому списку (список з будь-яких слів)
+l1 = ['Hello', 'my', 'hobby', 'is', 'coding']
+N = 3
+
+l1n = [i for i in l1 if len(i) > N]
+print(l1n)
+
+# робити новий список на основі двох початкових (конкатенація). 
+l2 = [1,2,3]
+l3 = [4,5,6]
+l4 = l2 + l3
+print(l4)
+
+
+students = [
+    ["Andriy", "NJNK"], 
+    ["sdgfsdf", "dfgdfg"], 
+    ["strsdf", "sdfsd"]
+    ]
+
+for name in students:
+    name[0] == 'Andriy'
